a5089cc82160602b149c83159f17da0f2f9d4c50
Marin <mrbumbac@gmail.com>
2020-02-01 15:51:50 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+
+  export default {
+    name: "App",
+    data () {
+      return {
+        img: ''
+      }
+    },
+    mounted() {
+      this.loadLogo()
+    },
+    methods: {
+      async loadLogo () {
+        this.img = (await import('@/assets/logo.png')).default
+      }
+    }
+  };
+</script>
+
+<template>
+ <div>
+   <img :src="img" alt="">
+   <div class="container">Hello world!</div>
+ </div>
+</template>
+
+<style scoped type="scss">
+  $red: #ff008c;
+
+  .container {
+    color: $red;
+    background: url("./assets/logo.png");
+    font-size: 65px;
+  }
+</style>
